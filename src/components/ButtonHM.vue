08c1585a997fae50184339dd8ea2a580f12da9fb
Lisa <lisakostre@gmail.com>
2022-12-08 17:21:02 +0100

    Add Button

---
+++ src/components/ButtonHM.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  props: {
+    text: String,
+    link: String,
+  },
+};
+</script>
+
+<template>
+  <a v-if="link" :href="link" class="button">{{ text }}</a>
+  <button v-else class="button">{{ text }}</button>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  padding: 0.5rem;
+  display: inline-block;
+  background: $HMColor;
+  color: $white;
+  border: 1px solid $HMColor;
+  border-radius: 2px;
+  margin: 0.5rem;
+  font-size: 1rem;
+  font-family: "Arial";
+  line-height: 1rem;
+  text-decoration:none;
+  &:hover {
+    color: $HMColor;
+    background: $white;
+  }
+  &.-Secondary{
+    color: $HMColor;
+    background: $white;
+    &:hover {
+        background: $HMColor;
+        color: $white;
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script>
 import NavBarHM from "../components/NavBarHM.vue";
 import FooterHM from "../components/FooterHM.vue";
+import ButtonHM from "../components/ButtonHM.vue";
 
 export default {
   components: {
     NavBarHM,
     FooterHM,
+    ButtonHM,
   },
 };
 </script>
@@ -16,6 +18,13 @@ export default {
     <h1>Caviar</h1>
     <h2>Rasputin</h2>
     <h3>Poppins</h3>
+    <ButtonHM
+    text="Bouton"
+    link="#" />
+    <ButtonHM
+    class="-Secondary"
+    text="Bouton"
+    link="#" />
     <FooterHM></FooterHM>
   </main>
 </template>
