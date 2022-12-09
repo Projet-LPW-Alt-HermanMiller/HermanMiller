3d83bb43f291252082535585a614e7067f4f1475
Lisa <lisakostre@gmail.com>
2022-12-09 17:24:06 +0100

    Formulaire Inscription début

---
+++ src/components/FormSignInHM.vue
@@ -0,0 +1,118 @@
+<script>
+import ButtonHM from "./ButtonHM.vue";
+export default {
+  data() {
+    return {
+      hasError: false,
+      showPassword: false,
+      email: "",
+      password: "",
+    };
+  },
+  components: {
+    ButtonHM,
+  },
+  methods: {
+    handlePasswordClick() {
+      this.showPassword = !this.showPassword;
+    },
+    handleSubmit() {
+      if (this.email.length === 0 || this.password.length === 0) {
+        this.hasError = true;
+      } else {
+        this.hasError = false;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="formLogIn">
+    <div class="fontH1">Inscription</div>
+
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <input
+          v-model="email"
+          name="email" 
+          type="text" 
+          placeholder="Adresse e-mail"
+        />
+      </div>
+      <div class="formLogIn__Identity">
+        <input
+          class="formLogIn__Identity__Input"
+          v-model="email"
+          name="email" 
+          type="text" 
+          placeholder="Adresse e-mail"
+        />
+        <input
+          class="formLogIn__Identity__Input"
+          v-model="password"
+          name="password"
+          :type="showPassword ? 'text' : 'password'"
+          placeholder="Mot de passe"
+        />
+      </div>
+      <div>
+        <input
+          v-model="password"
+          name="password"
+          :type="showPassword ? 'text' : 'password'"
+          placeholder="Mot de passe"
+        />
+      </div>
+      <div>
+        <input
+          v-model="newsletter"
+          type="checkbox"
+          id="newsletter"
+          name="newsletter"
+        />
+        <label for="newsletter">Je m'inscris à la newsletter</label>
+      </div>
+      <!-- Afficher ou masquer le mot de passe dans l'input -> à ajouter si besoin -->
+      <!-- <p @click="handlePasswordClick">
+        {{ showPassword ? "Masquer" : "Afficher" }} le mdp
+      </p> -->
+      <div>
+        <a href="#"> Mot de passe oublié ?</a>
+      </div>
+      <div v-if="hasError">
+        <p>Veuillez entrer toutes les informations</p>
+      </div>
+      <ButtonHM class="-Secondary">Inscription</ButtonHM>
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.formLogIn {
+  text-align: center;
+  &__Identity{
+    display: flex;
+    justify-content: space-between;
+    width: 30%;
+    margin: auto;
+    &__Input{
+      width: 45%;
+      margin: 0;
+    }
+  }
+}
+
+a {
+  text-decoration: none;
+  color: $darkGrey;
+  margin: rem(12);
+  &:visited {
+    color: $darkGrey;
+  }
+}
+
+input{
+  width: 30%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import NavBarHM from "../components/NavBarHM.vue";
 import FooterHM from "../components/FooterHM.vue";
 import ButtonHM from "../components/ButtonHM.vue";
 import FormLogInHM from "../components/FormLogInHM.vue";
+import FormSignInHM from "../components/FormSignInHM.vue";
 
 export default {
   components: {
@@ -10,6 +11,7 @@ export default {
     FooterHM,
     ButtonHM,
     FormLogInHM,
+    FormSignInHM,
   },
 };
 </script>
@@ -20,12 +22,11 @@ export default {
     <h1>Caviar</h1>
     <h2>Rasputin</h2>
     <h3>Poppins</h3>
-    <div>
-      dgeg "ger grgrg  et"g regrgzgzgz thtehzrg ztttezf tzetz
-    </div>
+    <div>dgeg "ger grgrg  et"g regrgzgzgz thtehzrg ztttezf tzetz</div>
     <ButtonHM>TEST 1</ButtonHM>
     <ButtonHM class="-Secondary">TEST 2343536454363</ButtonHM>
     <FormLogInHM />
+    <FormSignInHM />
     <FooterHM></FooterHM>
   </main>
 </template>
