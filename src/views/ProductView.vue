5664a6501a948015f2b02b585ad38d35da9c731d
nathanvanbignoot <nathan.vanbignoot@icloud.com>
2023-03-07 17:22:35 +0100

    Add : items on ProductView.vue

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -6,15 +6,17 @@
       </div>
       <div class="product-view__content">
         <h1 class="product-view__name">{{ displayedProduct.name }}</h1>
-        <span class="product-view__sku">{{ displayedProduct.sku }}</span>
-        <p class="product-view__price">{{ displayedProduct.price }}€</p>
+        <p class="product-view__price">{{ displayedProduct.price }}€ TTC</p>
+        <h2>Choix de la couleur</h2>
         <div v-if="colorAttribute" class="product-view__attribute">
           <div
             v-for="(option, index) in colorAttribute.options"
             class="product-view__option"
             @click="changeColor(option)"
           >
-            {{ option }}
+            <button-h-m>
+              {{ option }}
+            </button-h-m>
           </div>
         </div>
         <div class="product-view__description">
@@ -22,7 +24,7 @@
             v-if="displayedProduct.dimensions"
             class="product-view__dimensions"
           >
-            <p class="product-view__subtitle">Dimensions du produit :</p>
+            <h2 class="product-view__subtitle">Dimensions du produit :</h2>
             <ul>
               <li class="product-view__dimensions-item">
                 Longueur : {{ displayedProduct.dimensions.length }}cm
@@ -62,6 +64,15 @@
         </div>
       </div>
     </div>
+    <div>
+      <product-gallery-h-m
+        v-if="product !== {}"
+        :img_1="product.acf.gallery_product.image_1.url"
+        :img_2="product.acf.gallery_product.image_2.url"
+        :img_3="product.acf.gallery_product.image_3.url"
+      />
+      <our-chairs :product-data="products" />
+    </div>
   </div>
 </template>
 
@@ -71,12 +82,15 @@ import { client } from "@/utils/axios";
 import ProductGallery from "@/components/ProductGallery.vue";
 
 import ButtonHM from "@/components/ButtonHM.vue";
+import ProductGalleryHM from "@/components/productGalleryHM.vue";
+import OurChairs from "@/components/OurChairsHM.vue";
 
 export default {
-  components: { ButtonHM, ProductGallery },
+  components: { OurChairs, ProductGalleryHM, ButtonHM, ProductGallery },
   data() {
     return {
       product: {},
+      products: [],
       variations: [],
       activeColor: null,
       quantity: 1,
@@ -104,6 +118,14 @@ export default {
 
   async mounted() {
     await this.getProductData(this.$route.params.product);
+    console.log(this.product);
+
+    const productResponse = await client.get("/wc/v3/products");
+    this.products = productResponse.data;
+    console.log(this.products);
+  },
+  created() {
+    this.getProductData(this.$route.params.product);
   },
 
   async beforeRouteUpdate(to, from) {
@@ -159,17 +181,63 @@ export default {
     justify-content: space-between;
     align-items: center;
   }
+  &__attribute {
+    display: flex;
+  }
+  &__dimensions > ul {
+    margin-left: 0;
+    padding-left: 0;
+  }
+  &__dimensions-items {
+    list-style: none;
+    margin-left: 0;
+  }
   &__gallery {
-    width: 100%;
-    max-width: 500px;
+    width: 50%;
     margin-bottom: 2rem;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
   }
   &__content {
-    width: 100%;
-    max-width: 500px;
+    width: 50%;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
     padding: 2rem;
+  }
+  &__name {
+    margin: 0;
+    font-size: 3rem;
+  }
+  &__price {
+    margin-top: 0;
+    font: $secondary-title-font-family;
+    font-size: 1.5rem;
+  }
+  &__actions {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: start;
+    align-items: center;
+  }
+  &__quantity {
+    display: flex;
+    flex-direction: row;
+    width: 150px;
+    font-size: 24px;
+    border: inset 1px #000000;
+    border-radius: 2px;
+  }
+  &__quantity-value {
+    margin-left: auto;
+    margin-right: auto;
+  }
+  &__quantity-button {
+    width: 34px;
+    height: 34px;
+    border-radius: 2px;
+    color: white;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
   }
 }
 </style>
